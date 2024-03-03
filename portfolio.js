6ca88a72612398b2f2acfd4dca167ac1b2674008
zhongwenzhou <jojozhou0927@gmail.com>
2024-03-03 15:31:39 -0500

    A few more improvements

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,9 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
